b228814ed6435921cbc8578aec9bb5ee71799e9d
Falendrac <leger_franck@hotmail.fr>
2022-03-21 15:46:08 +0100

    Add task 16, is perfect.

--- binary_trees.h
+++ binary_trees.h
@@ -50,4 +50,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
+int perfect_tree(const binary_tree_t *tree, int depth, int level);
+
 #endif /* _BINARY_TREES_H_ */
