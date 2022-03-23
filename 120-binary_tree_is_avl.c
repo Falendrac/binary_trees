5baf91d6ec546098ee91b30d7084f4ac294b58dc
Falendrac <leger_franck@hotmail.fr>
2022-03-23 14:13:21 +0100

    Fix betty in 120.

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -27,25 +27,28 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
- * @brief
+ * check_avl - Check avl with int_min and int_max
  *
- * @param tree
- * @param int_min
- * @param int_max
- * @return int
+ * @tree: pointer to the root node of the tree to check
+ * @int_min: Minimum value for int
+ * @int_max: Maximum value for int
+ * Return: 0 or 1
  */
 int check_avl(const binary_tree_t *tree, int int_min, int int_max)
 {
 	size_t difference;
+	int heightLeft, heightRight;
 
 	if (tree != NULL)
 	{
 		if (tree->n < int_min || tree->n > int_max)
 			return (0);
-		if (binary_tree_height(tree->left) > binary_tree_height(tree->right))
-			difference = binary_tree_height(tree->left) - binary_tree_height(tree->right);
+		heightLeft = binary_tree_height(tree->left);
+		heightRight = binary_tree_height(tree->right);
+		if (heightLeft > heightRight)
+			difference = heightLeft - heightRight;
 		else
-			difference = binary_tree_height(tree->right) - binary_tree_height(tree->left);
+			difference = heightRight - heightLeft;
 		if (difference > 1)
 			return (0);
 		return (check_avl(tree->left, int_min, tree->n - 1) &&
@@ -55,14 +58,14 @@ int check_avl(const binary_tree_t *tree, int int_min, int int_max)
 }
 
 /**
- * binary_tree_is_avl :
+ * binary_tree_is_avl - checks if a binary tree is a valid AVL Tree
  *
- * @tree:
+ * @tree: pointer to the root node of the tree to check
  * Return: NULL or 1
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	return (check_avl(tree, -2147483647, 2147483647));
+	return (check_avl(tree, INT_MIN, INT_MAX));
 }
